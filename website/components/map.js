45fdbfd4f4568f77417a13f680e19f38f05b32cd
halvorlinder <halvor_lh@hotmail.no>
2024-05-23 08:37:24 +0200

    add selected player image in the map

--- website/components/map.js
+++ website/components/map.js
@@ -3,18 +3,7 @@ import * as utils from "../utils.js";
 
 const MAP_BADGE_WIDTH = 40;
 
-export const TRANSFER_COLORS = [
-  "#000000",
-  "#4a5079",
-  "#961C34",
-  "#93A80E",
-  "#4992E7",
-  "#e03022",
-  "#A9A7BC",
-  "#181B26",
-  "#ed6922",
-  "#4fa1c2",
-];
+export const TRANSFER_COLORS = ["#000000", "#4a5079", "#961C34", "#93A80E", "#4992E7", "#e03022", "#A9A7BC", "#181B26", "#ed6922", "#4fa1c2"];
 
 const svg = d3.select("#map");
 
@@ -55,18 +44,28 @@ export const draw = (
   let clubToPlayers = {};
 
   if (selectedPlayer || hoveredPlayer) {
-    const playerTransfers = transferData[
-      selectedPlayer?.name || hoveredPlayer?.name
-    ].data.filter((transfer) => transfer.year <= selectedYear);
+    const player = selectedPlayer || hoveredPlayer;
+    const playerTransfers = transferData[selectedPlayer?.name || hoveredPlayer?.name].data.filter((transfer) => transfer.year <= selectedYear);
 
     const filteredClubData = Object.keys(clubData)
-      .filter((clubName) =>
-        playerTransfers.some((t) => t.club_name == clubName)
-      )
-      .reduce(
-        (res, clubName) => ((res[clubName] = clubData[clubName]), res),
-        {}
-      );
+      .filter((clubName) => playerTransfers.some((t) => t.club_name == clubName))
+      .reduce((res, clubName) => ((res[clubName] = clubData[clubName]), res), {});
+
+    const playersClubs = [
+      {
+        club: transferData[player.name].data
+          .slice()
+          .reverse()
+          .find((transfer) => transfer.year <= selectedYear)?.club_name,
+        player: player,
+      },
+    ];
+    playersAndClubs = playersClubs.filter((pair) => pair.club);
+    clubToPlayers = Object.fromEntries(
+      Object.keys(clubData)
+        .map((club) => [club, playersClubs.filter((player) => player.club == club).map((player) => player.player)])
+        .filter((d) => d[1].length > 0)
+    );
 
     links = utils.ints(playerTransfers.length - 1).map((i) => {
       const fromClub = filteredClubData[playerTransfers[i].club_name];
@@ -93,19 +92,12 @@ export const draw = (
     });
     clubToPlayers = Object.fromEntries(
       Object.keys(clubData)
-        .map((club) => [
-          club,
-          playersClubs
-            .filter((player) => player.club == club)
-            .map((player) => player.player),
-        ])
+        .map((club) => [club, playersClubs.filter((player) => player.club == club).map((player) => player.player)])
         .filter((d) => d[1].length > 0)
     );
-    console.log(clubToPlayers);
     links = [];
     clubs = Object.entries(clubData).filter((d) => clubToPlayers[d[0]]);
-    // playersAndClubs = utils.zip2(playerData, playersClubs).filter(pair => pair[1].club)
-    playersAndClubs = playersClubs.filter(pair => pair.club);
+    playersAndClubs = playersClubs.filter((pair) => pair.club);
   }
 
   const size = svg.node().getBoundingClientRect().width;
@@ -117,51 +109,39 @@ export const draw = (
 
   drawCountries(mapData, projection);
 
-  drawClubs(
-    clubs,
-    clubData,
-    playersAndClubs,
-    clubToPlayers,
-    selectedClub,
-    hoveredClub,
-    setSelectedClub,
-    setHoveredClub,
-    projection
-  );
+  drawClubs(clubs, clubData, playersAndClubs, clubToPlayers, selectedClub, hoveredClub, setSelectedClub, setHoveredClub, projection);
 
   drawLinks(links, projection);
 };
 
 const player_x_offset = (index) => {
   switch (index) {
-    case 0: return -15;
-    case 1: return -30;
-    case 2: return 40;
-    default: return 50;
+    case 0:
+      return -15;
+    case 1:
+      return -30;
+    case 2:
+      return 40;
+    default:
+      return 50;
   }
-}
+};
 
 const player_y_offset = (index) => {
   switch (index) {
-    case 0: return -30;
-    case 1: return -15;
-    case 2: return 40;
-    default: return 50;
+    case 0:
+      return -30;
+    case 1:
+      return -15;
+    case 2:
+      return 40;
+    default:
+      return 50;
   }
-}
+};
 
-const drawClubs = (
-  clubs,
-  clubData,
-  playersAndClubs,
-  clubToPlayers,
-  selectedClub,
-  hoveredClub,
-  setSelectedClub,
-  setHoveredClub,
-  projection
-) => {
-  console.log(clubToPlayers)
+const drawClubs = (clubs, clubData, playersAndClubs, clubToPlayers, selectedClub, hoveredClub, setSelectedClub, setHoveredClub, projection) => {
+  console.log(clubToPlayers);
   svg
     .select("#players")
     .selectAll("image")
@@ -170,48 +150,33 @@ const drawClubs = (
     .attr("href", (d) => d.player.portrait_src)
     .attr(
       "x",
-      (d) => projection([clubData[d.club].long, clubData[d.club].lat])[0] - MAP_BADGE_WIDTH / 2 + player_x_offset(clubToPlayers[d.club].findIndex((player) => d.player.name == player.name))
+      (d) =>
+        projection([clubData[d.club].long, clubData[d.club].lat])[0] -
+        MAP_BADGE_WIDTH / 2 +
+        player_x_offset(clubToPlayers[d.club].findIndex((player) => d.player.name == player.name))
     )
     .attr(
       "y",
-      (d) => projection([clubData[d.club].long, clubData[d.club].lat])[1] - MAP_BADGE_WIDTH / 2 + player_y_offset(clubToPlayers[d.club].findIndex((player) => d.player.name == player.name))
+      (d) =>
+        projection([clubData[d.club].long, clubData[d.club].lat])[1] -
+        MAP_BADGE_WIDTH / 2 +
+        player_y_offset(clubToPlayers[d.club].findIndex((player) => d.player.name == player.name))
     )
-    .attr("width", (d) =>
-      40
-    )
-    .attr("height", (d) =>
-      40
-    )
+    .attr("width", (d) => 40)
+    .attr("height", (d) => 40);
   svg
     .select("#clubs")
     .selectAll("image")
     .data(clubs)
     .join("image")
     .attr("href", (d) => d[1].image.src)
-    .attr(
-      "x",
-      (d) => projection([d[1].long, d[1].lat])[0] - MAP_BADGE_WIDTH / 2
-    )
-    .attr(
-      "y",
-      (d) => projection([d[1].long, d[1].lat])[1] - MAP_BADGE_WIDTH / 2
-    )
-    .attr("opacity", (d) =>
-      selectedClub == null || selectedClub == d[0] || hoveredClub == d[0]
-        ? 1
-        : 0.7
-    )
-    .classed(
-      "club-badge-selected",
-      (d) => selectedClub == d[0] || hoveredClub == d[0]
-    )
+    .attr("x", (d) => projection([d[1].long, d[1].lat])[0] - MAP_BADGE_WIDTH / 2)
+    .attr("y", (d) => projection([d[1].long, d[1].lat])[1] - MAP_BADGE_WIDTH / 2)
+    .attr("opacity", (d) => (selectedClub == null || selectedClub == d[0] || hoveredClub == d[0] ? 1 : 0.7))
+    .classed("club-badge-selected", (d) => selectedClub == d[0] || hoveredClub == d[0])
     // These are mutually exclusive
-    .attr("width", (d) =>
-      d[1].image.width >= d[1].image.height ? MAP_BADGE_WIDTH : null
-    )
-    .attr("height", (d) =>
-      d[1].image.width < d[1].image.height ? MAP_BADGE_WIDTH : null
-    )
+    .attr("width", (d) => (d[1].image.width >= d[1].image.height ? MAP_BADGE_WIDTH : null))
+    .attr("height", (d) => (d[1].image.width < d[1].image.height ? MAP_BADGE_WIDTH : null))
     .on("click", (_, d) => setSelectedClub(d[0]))
     .on("mouseenter", (_, d) => setHoveredClub(d[0]))
     .on("mouseleave", (_) => setHoveredClub(null));
@@ -236,9 +201,6 @@ const drawCountries = (mapData, projection) => {
     .selectAll("path")
     .data(mapData)
     .join("path")
-    .attr(
-      "fill",
-      getComputedStyle(document.documentElement).getPropertyValue("--bg")
-    )
+    .attr("fill", getComputedStyle(document.documentElement).getPropertyValue("--bg"))
     .attr("d", d3.geoPath().projection(projection));
 };
