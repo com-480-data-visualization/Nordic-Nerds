cbc2f2af829e0f4582b595e7ce64c4bc8d2ea267
halvorlinder <halvor_lh@hotmail.no>
2024-05-23 09:32:25 +0200

    add description to heatmep

--- website/components/events.js
+++ website/components/events.js
@@ -5,124 +5,108 @@ const buttons = events.selectAll(".event-button");
 const svg = events.select("#field-map");
 const fieldLines = svg.select("#field-lines");
 
-const MARGIN = {horizontal: 20, vertical: 20};
+const MARGIN = { horizontal: 20, vertical: 20 };
 
 export const setup = () => {
-    fieldLines.attr("href", "images/soccer_field.svg");
-    resizeFieldLines();
+  fieldLines.attr("href", "images/soccer_field.svg");
+  resizeFieldLines();
 };
 
 const getSize = () => {
-    const size = svg.node().getBoundingClientRect();
-    return {
-        width: size.width - MARGIN.horizontal * 2,
-        height: size.height - MARGIN.vertical * 2,
-    };
+  const size = svg.node().getBoundingClientRect();
+  return {
+    width: size.width - MARGIN.horizontal * 2,
+    height: size.height - MARGIN.vertical * 2,
+  };
 };
 
 const resizeFieldLines = () => {
-    const {width, height} = getSize();
-    fieldLines
-        .attr("width", width)
-        .attr("height", height)
-        .attr("x", MARGIN.horizontal)
-        .attr("y", MARGIN.vertical);
+  const { width, height } = getSize();
+  fieldLines.attr("width", width).attr("height", height).attr("x", MARGIN.horizontal).attr("y", MARGIN.vertical);
 };
 
-export const draw = (
-    selectedYear,
-    selectedPlayer,
-    selectedClub,
-    selectedEvents,
-    eventsData,
-    setSelectedEvents
-) => {
-    if (selectedPlayer){
-        resizeFieldLines();
-        drawEventButtons(selectedEvents, setSelectedEvents);
-        drawEventHeatmap(selectedYear, selectedPlayer, selectedClub, selectedEvents, eventsData);
-    }
+export const draw = (selectedYear, selectedPlayer, selectedClub, selectedEvents, eventsData, setSelectedEvents) => {
+  if (!selectedPlayer) {
+    updateText("Select a player to see how his goalscoring ability has evolved over time");
+    drawEventHeatmap([]);
+    return;
+  }
+  const data = filterData(selectedYear, selectedPlayer, selectedClub, eventsData);
+  const nGoals = data.filter((datum) => datum.is_goal).length;
+  updateText(`${selectedPlayer.name} scored ${nGoals} goals in ${data.length - nGoals} attempts for ${selectedClub} in ${selectedYear}!`);
+  resizeFieldLines();
+  drawEventButtons(selectedEvents, setSelectedEvents);
+  drawEventHeatmap(filterEvents(selectedEvents, data));
+};
+
+const updateText = (text) => {
+  const textElem = document.getElementById("events-description");
+  textElem.innerHTML = text;
 };
 
 const drawEventButtons = (selectedEvents, setSelectedEvents) => {
-    buttons.on("click", (event) => {
-        let buttonId = event.target.id;
-        let button = d3.select("#" + buttonId);
-        if (selectedEvents.has(buttonId)) {
-            button.classed("event-button-selected", false);
-        } else {
-            button.classed("event-button-selected", true);
-        }
-        setSelectedEvents(buttonId);
-    });
+  buttons.on("click", (event) => {
+    let buttonId = event.target.id;
+    let button = d3.select("#" + buttonId);
+    if (selectedEvents.has(buttonId)) {
+      button.classed("event-button-selected", false);
+    } else {
+      button.classed("event-button-selected", true);
+    }
+    setSelectedEvents(buttonId);
+  });
 };
 
-function filterData(
-    selectedYear,
-    selectedPlayer,
-    selectedClub,
-    selectedEvents,
-    eventsData
-) {
-const data = eventsData
-    .filter(d => {return selectedYear == null ? true : d.season == selectedYear})
-    .filter(d => {return selectedPlayer == null ? true : d.player == selectedPlayer.name})
-    .filter(d => {return selectedClub == null ? true : d.event_team == selectedClub})
-    .filter(d => {return selectedEvents.has("goal-button") ? d.is_goal : true})
-    .filter(d => {return selectedEvents.has("miss-button") ? !d.is_goal : true});
-
-    if (data.size != 0) {
-        return Promise.resolve(data);
-    } else {
-        return Promise.reject("No data found.");
-    }
+function filterData(selectedYear, selectedPlayer, selectedClub, eventsData) {
+  const data = eventsData
+    .filter((d) => {
+      return selectedYear == null ? true : d.season == selectedYear;
+    })
+    .filter((d) => {
+      return selectedPlayer == null ? true : d.player == selectedPlayer.name;
+    })
+    .filter((d) => {
+      return selectedClub == null ? true : d.event_team == selectedClub;
+    });
+  return data;
 }
 
-const drawEventHeatmap = (
-    selectedYear,
-    selectedPlayer,
-    selectedClub,
-    selectedEvents,
-    eventsData
-) => {
-    const {width, height} = getSize();
-    filterData(selectedYear, selectedPlayer, selectedClub, selectedEvents, eventsData)
-        .then(
-            (data) => {
-            // Add X axis
-            const x = d3
-                .scaleLinear(d3.extent(data, d => d.x))
-                .range([MARGIN.horizontal, width - MARGIN.horizontal]);
+function filterEvents(selectedEvents, eventsData) {
+  const data = eventsData
+    .filter((d) => {
+      return selectedEvents.has("goal-button") ? d.is_goal : true;
+    })
+    .filter((d) => {
+      return selectedEvents.has("miss-button") ? !d.is_goal : true;
+    });
+  return data;
+}
 
-            const y = d3
-                .scaleLinear(d3.extent(data, d => d.y))
-                .range([height - MARGIN.vertical, MARGIN.vertical]);
+const drawEventHeatmap = (data) => {
+  const { width, height } = getSize();
+  // Add X axis
+  const x = d3.scaleLinear(d3.extent(data, (d) => d.x)).range([MARGIN.horizontal, width - MARGIN.horizontal]);
 
-            // Prepare a color palette
-            const color = d3
-                .scaleLinear([0.1, 1], ["blue", "#69b3a2"]);
+  const y = d3.scaleLinear(d3.extent(data, (d) => d.y)).range([height - MARGIN.vertical, MARGIN.vertical]);
 
-            // compute the density data
-            const densityData = d3
-                .contourDensity()
-                .x((d) => x(d.x))
-                .y((d) => y(d.y))
-                .size([width, height])
-                .bandwidth(10)(data);
+  // Prepare a color palette
+  const color = d3.scaleLinear([0.1, 1], ["blue", "#69b3a2"]);
 
-            d3.select("#heatmap")
-                .selectAll("path")
-                .data(densityData)
-                .join("path")
-                .attr("d", d3.geoPath())
-                .attr("fill", function (d) {
-                    return color(d.value);
-                })
-                .attr("opacity", 0.2);
-        },
-            (error) => {
-                console.log(error);
-                d3.select("#heatmap").selectAll("path").remove();
-            }
-            )
+  // compute the density data
+  const densityData = d3
+    .contourDensity()
+    .x((d) => x(d.x))
+    .y((d) => y(d.y))
+    .size([width, height])
+    .bandwidth(10)(data);
+
+  d3.select("#heatmap")
+    .selectAll("path")
+    .data(densityData)
+    .join("path")
+    .attr("d", d3.geoPath())
+    .attr("fill", function (d) {
+      return color(d.value);
+    })
+    .attr("opacity", 0.2);
 };
